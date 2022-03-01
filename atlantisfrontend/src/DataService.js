ada6e505842d9eb04dbfe8313256f55f751056c9
Patricia Mazuelo <patriciamazuelo@gmail.com>
2022-03-01 17:17:45 +0100

    Listado y crear anuncio

--- atlantisfrontend/src/DataService.js
+++ atlantisfrontend/src/DataService.js
@@ -1,6 +1,12 @@
 import client from "./api/client";
 
 export const getAdsList = () => {
-    const url = "/ads";
+    const url = "/api/ads";
     return client.get(url)
+}
+
+export const createAd = (adObj) => {
+    const url = "/api/ads";
+    console.log(adObj)
+    return client.post(url, adObj)
 }
