76db252ad66301a51952f9697e49911e89efa254
Patricia Mazuelo <patriciamazuelo@gmail.com>
2022-03-13 08:56:23 +0100

    arreglando estilos y errores

--- atlantisfrontend/src/components/Ad.js
+++ atlantisfrontend/src/components/Ad.js
@@ -1,59 +1,33 @@
+import "../css/Ad.css";
 export const Ad = (ad) => {
   return (
-    <div className="col-md-6 col-lg-3" key={ad.id}>
+    <div className=" d-flex flex-row ad-wrapper" key={ad.id}>
       <div className="pdt-item-blk mb-4">
-        <div className="pdt-img-blk">
-          <img
-            src="assets/img/pdt-iphone-img-1.jpg"
-            alt="Winter Coat with Hat"
-            className="img-fluid product-item-image"
-          ></img>
+        <div className="img">
+          {ad.imagen && (
+            <img
+              src={`http://localhost:3002${ad.imagen}`}
+              alt={ad.nombre}
+              width="300px"
+              height="300px"
+            ></img>
+          )}
         </div>
-        <div className="pdt-content-blk pt-0 pl-3 pr-3 pb-3">
-          <div className="position-relative">
-            <div className="buy-blk position-absolute r-0">
-              <ul className="pdt-item list-inline">
-                <li className="list-inline-item align-middle">
-                  <a href="javascript" className="text-grey">
-                    <span
-                      className="size-xs"
-                      data-feather="shopping-cart"
-                    ></span>
-                  </a>
-                </li>
-                <li className="list-inline-item align-middle">
-                  <a href="javascript" className="text-grey">
-                    <span className="size-xs" data-feather="heart"></span>
-                  </a>
-                </li>
-                <li className="list-inline-item align-middle">
-                  <a className="text-grey" href="javascript">
-                    <span className="size-xs" data-feather="eye"></span>
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <h6 className="mb-0">{ad.usuario}</h6>
+        <p className="ad-sale">{ad.venta ? "buy" : "sell"}</p>
+        <p className="ad-tags">{ad.tags}</p>
+        <div className="content-top-blk pt-3">
+          <div className="d-flex justify-content-between align-items-center">
+            <p className="text-warning font-bold">{ad.descripcion}</p>
           </div>
-          <div className="content-top-blk pt-3">
-            <div className="d-flex justify-content-between align-items-center">
-              <p className="text-warning font-bold">{ad.description}</p>
-            </div>
-            <h5 className="text-theme font-amt font-bold">{ad.precio}</h5>
-            <h4>
-              <a href="single-product.html" className="display-block text-link">
-                {ad.nombre}
-              </a>
-            </h4>
-          </div>
-          <div className="content-btm-blk">
-            <div className="media py-3">
-              <div className="media-body">
-                <h6 className="mb-0">{ad.usuario}</h6>
-              </div>
-            </div>
-            <p className="text-green font-bold"></p>
-          </div>
+          <h5 className="text-theme font-amt font-bold">{ad.precio}</h5>
+          <h4>
+            <a href="#" className="display-block text-link">
+              {ad.nombre}
+            </a>
+          </h4>
         </div>
+        <p className="text-green font-bold"></p>
       </div>
     </div>
   );
--- atlantisfrontend/src/components/NewAdPage.js
+++ atlantisfrontend/src/components/NewAdPage.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { createAd, getTags } from "../DataService";
+import Layout from "../layout/Layout";
 import FormField from "./FormField";
+import Button from "./common/button"
+
+import "./NewAdPage.css"
 
 export function NewAdPage({ history }) {
   const [ad, setAd] = useState({
@@ -44,91 +48,92 @@ export function NewAdPage({ history }) {
   const { results } = tags;
 
   return (
-    <div className="new-ad-page">
-      Create your ad...
-      <div className="create-ad-wrapper">
-        What do you want to sell/buy today?
-        <form className="create-ad-form" onSubmit={handleSubmit}>
-          <FormField
-            type="text"
-            name="nombre"
-            placeholder="What do you want to sell today?"
-            label="Name of your item"
-            className="nombre"
-            value={ad.nombre}
-            onChange={handleChange}
-          ></FormField>
-          <FormField
-            type="file"
-            name="imagen"
-            placeholder="Upload a picture of your item"
-            label="Imagen"
-            className="imagen"
-            value={ad.imagen}
-            onChange={handleChange}
-          ></FormField>
-          <FormField
-            type="radio"
-            name="venta"
-            placeholder="Are you selling?"
-            label="Sale"
-            className="venta"
-            value={true}
-            checked={ad.venta === true}
-            onChange={handleChange}
-          ></FormField>
-          <FormField
-            type="radio"
-            name="venta"
-            placeholder="are you buying?"
-            label="Buy"
-            className="venta"
-            value={false}
-            checked={ad.venta === false}
-            onChange={handleChange}
-          ></FormField>
-          <FormField
-            type="number"
-            name="precio"
-            placeholder="What price do you want to set? Don't be greedy!"
-            label="Price of your item"
-            className="precio"
-            value={ad.precio}
-            onChange={handleChange}
-          ></FormField>
-          <FormField
-            type="text"
-            name="descripcion"
-            placeholder="Tell to the world a bit your item and why is the best"
-            label="Description of your item"
-            className="descripcion"
-            value={ad.descripcion}
-            onChange={handleChange}
-          ></FormField>
-          <select name="tags" className="form-select" multiple>
-            {tags
-              ? results?.map((tag) => (
-                  <option key={tag} value={tag}>
-                    {tag}
-                  </option>
-                ))
-              : []}
-          </select>
-          <FormField
-            type="text"
-            name="usuario"
-            placeholder="What's your name?"
-            label="Name of yourself"
-            className="usuario"
-            value={ad.usuario}
-            onChange={handleChange}
-          ></FormField>
-          <button className="button" type="submit" variant="primary">
-            Create Ad
-          </button>
-        </form>
+    <Layout>
+      <div className="new-ad-page">
+        Create your ad...
+        <div className="create-ad-wrapper">
+          What do you want to sell/buy today?
+          <form className="create-ad-form" onSubmit={handleSubmit}>
+            <FormField
+              type="text"
+              name="nombre"
+              placeholder="What do you want to sell today?"
+              label="Name of your item"
+              className="nombre"
+              value={ad.nombre}
+              onChange={handleChange}
+            ></FormField>
+            <FormField
+              type="file"
+              name="imagen"
+              placeholder="Upload a picture of your item"
+              label="Imagen"
+              className="imagen"
+              value={ad.imagen}
+              onChange={handleChange}
+            ></FormField>
+            <FormField
+              type="radio"
+              name="venta"
+              placeholder="Are you selling?"
+              label="Sell"
+              className="venta"
+              value={ad.venta === true}
+              checked={true}
+              onChange={handleChange}
+            ></FormField>
+            <FormField
+              type="radio"
+              name="venta"
+              placeholder="Are you buying?"
+              label="Buy"
+              className="venta"
+              value={ad.venta === false}
+              onChange={handleChange}
+            ></FormField>
+            <FormField
+              type="number"
+              name="precio"
+              placeholder="What price do you want to set? Don't be greedy!"
+              label="Price of your item"
+              className="precio"
+              value={ad.precio}
+              onChange={handleChange}
+            ></FormField>
+            <FormField
+              type="text"
+              name="descripcion"
+              placeholder="Tell to the world a bit your item and why is the best"
+              label="Description of your item"
+              className="descripcion"
+              value={ad.descripcion}
+              onChange={handleChange}
+            ></FormField>
+            <select name="tags" className="form-select" multiple>
+              {tags
+                ? results?.map((tag) => (
+                    <option key={tag} value={tag}>
+                      {tag}
+                    </option>
+                  ))
+                : []}
+            </select>
+            <FormField
+              type="text"
+              name="usuario"
+              placeholder="What's your name?"
+              label="Name of yourself"
+              className="usuario"
+              value={ad.usuario}
+              onChange={handleChange}
+            ></FormField>
+            <Button className="button" type="submit" variant="primary">
+              Create Ad
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
