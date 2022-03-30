4d40346f7575812bd1e1622ac383175c18e2bf67
Heinz <hlanaumann@gmail.com>
2022-03-30 10:08:25 +0200

    par de toques esteticos

--- atlantisfrontend/src/components/Detail.js
+++ atlantisfrontend/src/components/Detail.js
@@ -34,29 +34,7 @@ function Detail({ match, history }) {
               <div className="row">
                 <div className="col-12">
                   <div className="single-pdt-wrapper">
-                    <div className="pdt-title-info mb-4 bg-white">
-                      <div className="row d-flex justify-content-center text-lg-center">
-                        <div className="col-lg-4">
-                          <div className="pdt-name-blk py-4 px-4 px-lg-0">
-                            <span
-                              data-feather="box"
-                              className="text-theme mr-3"
-                            ></span>
-                          </div>
-                        </div>
-                        <div className="col-lg-4">
-                          <div className="pdt-rate-blk rating-blk py-4 px-4 px-lg-0"></div>
-                        </div>
-                        <div className="col-lg-4">
-                          <div className="pdt-name-blk py-4 px-4 px-lg-0">
-                            <span
-                              data-feather="user-check"
-                              className="text-theme mr-3"
-                            ></span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                  
                     <div className="row">
                       <div className="col-lg-7 col-xl-7 mt-2 mt-md-5 mt-lg-0">
                         <div className="pdt-item-showcase bg-white">
@@ -74,17 +52,24 @@ function Detail({ match, history }) {
                       <div className="col-lg-5 col-xl-5">
                         <aside className="sidebar sidebar-user mt-4 mt-lg-0">
                           <div className="pdt-info-card bg-white p-4">
+                             <h1 className=" text-primary text-grey d-inline-block align-middle">
+                                
+                                {advert.nombre}
+                            </h1>
+                             <hr className="my-4" />
                             <div className="price-blk">
                               <div className="d-block d-lg-flex justify-content-between align-items-center mt-4">
-                                <h2 className="font-bold text-theme">
-                                  Price {advert.precio}€
-                                </h2>
+                                <h3 className="text-secondary font-bold text-theme">
+                                  Price  {advert.precio}€
+                                </h3>
                               </div>
                             </div>
                             <hr className="my-4" />
-                            <p className="overflow-auto d-inline-block text-grey align-middle">
+                              <div className="d-flex mw-50 flex-wrap justify-content-between align-items-center mt-4">
+                                 <p className=" overflow-auto d-flex flex-wrap mw-50">
                               {advert.descripcion}
-                            </p>
+                              </p>
+                              </div>
                             <hr className="my-4" />
                             <div className="add-to-cart-blk">
                               <div className="row">
@@ -105,7 +90,7 @@ function Detail({ match, history }) {
                                   )}
                                   <div className="row">
                                     <div className="col-md-12">
-                                      <h5 className="font-bold text-theme">
+                                      <h5 className="font-bold text-theme mt-2">
                                         Share to peers
                                       </h5>
                                       <a>
@@ -114,17 +99,20 @@ function Detail({ match, history }) {
                                         />
                                       </a>
                                     </div>
+                                    
                                   </div>
                                 </div>
                               </div>
+                              
                               <div className="d-block d-lg-flex justify-content-between mt-4"></div>
                               <hr className="my-4" />
                               <h5 className="text-grey d-inline-block align-middle">
-                                Username: <br />
-                                {advert.nombre}
+                                <Icon.User />
+                                {advert.usuario_nombre}
                               </h5>
                             </div>
                           </div>
+                     
 
                           <div className="share-info-card bg-white mt-4 p-4">
                             <h5 className="text-grey d-inline-block align-middle">
